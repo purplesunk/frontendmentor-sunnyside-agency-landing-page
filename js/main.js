5c1ebfd103d3f8c7adc68017345b63b8129e2059
Juan Ignacio Marín <ignacio.bendwise@aleeas.com>
2021-06-27 17:22:01 -0300

    eso

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll("[data-button-target]")
+
+buttons.forEach(button => button.addEventListener('click', _ => {
+  const target = button.dataset.buttonTarget
+  const menu = document.querySelector(target)
+  menu.classList.toggle("active")
+})
+)
